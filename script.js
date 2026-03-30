af778bb912098c804eb87bd145293e8731a3a9d5
Lemonka5212 <57177735+ilyalemon777ibragimov@users.noreply.github.com>
2026-03-30 11:28:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function login() {
 	if (document.getElementById('l').value==="admin" && document.getElementById('p'). value==="1") {
-	window.location.href = 'https://youtu.be/dQw4w9WgXcQ?si=W9r8sioaS0k5wh8A;}
+	window.location.href = 'https://youtu.be/dQw4w9WgXcQ?si=W9r8sioaS0k5wh8A;'
+	}
 	else {
 		alert('Invalid login or password');
 		}
