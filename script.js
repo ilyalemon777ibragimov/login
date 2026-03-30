921a104d7ac065558475d3700b90a5b705bd6d90
Lemonka5212 <57177735+ilyalemon777ibragimov@users.noreply.github.com>
2026-03-30 11:17:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function login() {
+	if (document.getElementById('l').value==="admin" && document.getElementById('p'). value==="1") {
+	window.location.href = 'https://youtu.be/dQw4w9WgXcQ?si=W9r8sioaS0k5wh8A;}
+	else {
+		alert('Invalid login or password');
+		}
+	}
+		
