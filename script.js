b44fc7c3dab1e4b86a734293278ff31328cc4109
Lemonka5212 <57177735+ilyalemon777ibragimov@users.noreply.github.com>
2026-03-30 11:43:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function login() {
-	if (document.getElementById('l').value==="admin" && document.getElementById('p'). value==="1") {
+	if (document.getElementById('l').value==="admin" && document.getElementById('p').value==="1") {
 	window.location.href = 'https://youtu.be/dQw4w9WgXcQ?si=W9r8sioaS0k5wh8A;'
 	}
-	else {
-		alert('Invalid login or password');
+	else if (document.getElementById('l').value==="kurantechan" && document.getElementById('p').value==="kurantechan") {
+	window.location.href = 'https://youtu.be/dQw4w9WgXcQ?si=W9r8sioaS0k5wh8A;'}
+    else {
+			alert('Invalid login or password');
 		}
 	}
-		
